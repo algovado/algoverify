60f69cbfb0d4023b151ec8a49257ae652e6eb345
algovado <90692255+algovado@users.noreply.github.com>
2025-01-30 14:33:07 +0300

    update nodely urls

--- backend/app/constants.ts
+++ backend/app/constants.ts
@@ -9,5 +9,5 @@ export const BASE_URL = process.env.BASE_URL || "";
 
 export const APPLY_FEE = parseFloat(process.env.APPLY_FEE || "1");
 export const APPLY_WALLET = process.env.APPLY_WALLET || "VERFYVVV6UY6BSURMMQ23MOAOF7GD6DJ676EQZYOKJTJLLLFPJ4Q5VF3VU";
-export const ALGONODE_MAINNET_NODE_URL = process.env.MAINNET_NODE || "https://mainnet-api.algonode.cloud";
-export const ALGONODE_IDX_NODE_URL = process.env.MAINNET_INDEXER || "https://mainnet-idx.algonode.cloud/v2";
+export const ALGONODE_MAINNET_NODE_URL = process.env.MAINNET_NODE || "https://mainnet-api.4160.nodely.dev";
+export const ALGONODE_IDX_NODE_URL = process.env.MAINNET_INDEXER || "https://mainnet-idx.4160.nodely.dev";
